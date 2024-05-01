1c098df51bba8e9f3e10322dec05df90b68006e2
Misbakhul Munir <74493085+hazart29@users.noreply.github.com>
2024-05-02 03:52:04 +0700

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <link rel="manifest" href="/manifest.json" />
+      <link crossOrigin="use-credentials" rel="manifest" href="/manifest.json" />
       <body className={inter.className}>
         <Analytics />
         <SpeedInsights />
