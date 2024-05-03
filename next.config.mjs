521ad4befa1f4a67def9b96dd9063a22b28a4ce0
Misbakhul Munir <74493085+hazart29@users.noreply.github.com>
2024-05-03 19:28:25 +0700

    up

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,19 @@ import {
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: ['/', '/main'],
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=1, stale-while-revalidate=59',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 const nextConfigFunction = async (phase) => {
