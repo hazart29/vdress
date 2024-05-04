f864d934f27a45f7ae68e35504f3221223c20349
Misbakhul Munir <74493085+hazart29@users.noreply.github.com>
2024-05-04 22:00:07 +0700

    up

--- src/app/api/db/tambahPity/route.tsx
+++ src/app/api/db/tambahPity/route.tsx
@@ -3,6 +3,6 @@ import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-    await sql`UPDATE players SET pity_counter = 90`;
+    await sql`UPDATE players SET pitycounter = 90`;
     return NextResponse.json({message: 'sukses tambah pity semua player 90'}, {status: 200});
 }
