8a489b697a4875bc9963e46ce6d34427c323cad6
Misbakhul Munir <74493085+hazart29@users.noreply.github.com>
2024-05-15 16:46:16 +0700

    up2

--- src/app/api/auth/route.tsx
+++ src/app/api/auth/route.tsx
@@ -1,5 +1,4 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import pool from '../../db';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'
 import { sql } from '@vercel/postgres';
