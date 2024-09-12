648a9076014e906d06ecfaf622d3e140af46a44a
Misbakhul Munir <74493085+hazart29@users.noreply.github.com>
2024-09-12 14:15:17 +0700

    update 12/09/2024

--- src/app/api/db/tambahPity/route.tsx
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { sql } from '@vercel/postgres';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-    await sql`UPDATE users SET pitycounter = 90`;
-    return NextResponse.json({message: 'sukses tambah pity semua player 90'}, {status: 200});
-}
--- src/app/api/db/tambahPrimo/route.tsx
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { sql } from '@vercel/postgres';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-    await sql`UPDATE users SET primogems = 99999`;
-    return NextResponse.json({message: 'sukses tambah primo semua player 99999'}, {status: 200});
-}
