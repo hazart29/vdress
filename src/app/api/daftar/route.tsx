08bced0a01c2677cbc0dc58b144c186d24aad80d
Misbakhul Munir <74493085+hazart29@users.noreply.github.com>
2024-12-24 19:02:44 +0700

    24/12/2024 v3

--- src/app/api/daftar/route.tsx
+++ src/app/api/daftar/route.tsx
@@ -1,78 +1,60 @@
-// pages/api/daftar.ts
 import { NextApiRequest, NextApiResponse } from 'next';
 import bcrypt from 'bcrypt';
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 import sjcl from 'sjcl';
 
-const pwd = process.env.SJCL_PASSWORD; // Retrieve password from environment variables
+const pwd = process.env.SJCL_PASSWORD;
 
 export async function GET() {
-  try {
-    const rows = await sql`SELECT * FROM users`;
+    try {
+        const rows = await sql`SELECT * FROM users`;
 
-    if (rows.rows) {
-      return NextResponse.json({ status: "success", message: 'Successed getting api data', statusCode: 200 }, { status: 200 });
-    } else {
-      return NextResponse.json({ status: "notFound", message: 'Data not found', errorCode: 404 }, { status: 404 });
+        if (rows.rows) {
+            return NextResponse.json({ status: "success", message: 'Successed getting api data', statusCode: 200 }, { status: 200 });
+        } else {
+            return NextResponse.json({ status: "notFound", message: 'Data not found', errorCode: 404 }, { status: 404 });
+        }
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ status: "internalError", message: 'Internal server error', errorCode: 500 }, { status: 500 });
     }
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ status: "internalError", message: 'Internal server error', errorCode: 500 }, { status: 500 });
-  }
 }
 
 export async function POST(req: Request) {
-  const { encryptedData } = await req.json();
-  const decryptedData = JSON.parse(sjcl.decrypt(pwd as string, encryptedData));
-  const { username, password, email, name } = decryptedData;
+    const { encryptedData } = await req.json();
+    const decryptedData = JSON.parse(sjcl.decrypt(pwd as string, encryptedData));
+    const { username, password, email, name } = decryptedData;
 
-  try {
-    // Validate input
-    if (!username || !password || !email || !name) {
-      return NextResponse.json({ message: 'Missing required fields' }, { status: 400 });
-    }
+    try {
+        if (!username || !password || !email || !name) {
+            return NextResponse.json({ message: 'Missing required fields' }, { status: 400 });
+        }
 
-    // Check if user already exists
-    const existingUser = await sql`SELECT * FROM users WHERE email = ${email} OR username = ${username}`;
-    if (existingUser.rowCount > 0) {
-      return NextResponse.json({ message: 'User already exists' }, { status: 400 });
-    }
+        const existingUser = await sql`SELECT * FROM users WHERE email = ${email} OR username = ${username}`;
+        if (existingUser.rowCount > 0) {
+            return NextResponse.json({ message: 'User already exists' }, { status: 400 });
+        }
 
-    // Hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Save new user to database using parameterized query
-    const { rows: users } = await sql`
-      INSERT INTO users (username, password, email, name)
-      VALUES (${username}, ${hashedPassword}, ${email}, ${name})
-      RETURNING *
-    `;
-
-    const newUser = users[0];
-
-    if (newUser) {
-      try {
-        await sql`
-          INSERT INTO user_resources (uid)
-          VALUES (${newUser.uid}) 
-          RETURNING *
+        const { rows: users } = await sql`
+            INSERT INTO users (username, password, email, name)
+            VALUES (${username}, ${hashedPassword}, ${email}, ${name})
+            RETURNING *
         `;
 
-        const encryptedReturnData = sjcl.encrypt(pwd as string, JSON.stringify(newUser));
-        return NextResponse.json({encryptedReturnData: encryptedReturnData}, { status: 201 });
-      } catch (error) {
-        // If there's an error creating the user resource, delete the user
-        console.error('Error creating user resource:', error);
-        await sql`DELETE FROM users WHERE uid = ${newUser.uid}`;
-        return NextResponse.json({ message: 'Failed to create user resource. User registration rolled back.' }, { status: 500 });
-      }
-    } else {
-      return NextResponse.json({ message: 'Failed to create user' }, { status: 500 });
-    }
+        const newUser = users[0];
 
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
-  }
+        if (newUser) {
+            const encryptedReturnData = sjcl.encrypt(pwd as string, JSON.stringify(newUser));
+            return NextResponse.json({ encryptedReturnData: encryptedReturnData }, { status: 201 });
+        } else {
+            return NextResponse.json({ message: 'Failed to create user' }, { status: 500 });
+        }
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
+    }
 }
